eab698dbc6a0d46640e21363996cccf3b9fb998d
Evey <v4vlviv@gmail.com>
2018-09-02 23:01:15 +0300

    add project structure

---
+++ FreeCrmRuFramework/LoginPageTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace FreeCrmRuFramework
+{
+    [TestFixture]
+    public class LoginPageTest
+    {
+        [Test]
+        public void LoginTest()
+        {
+        }
+    }
+}
---
+++ FreeCrmRuFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FreeCrmRuFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FreeCrmRuFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7174771a-e7db-439d-aa0b-93304e15107b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
